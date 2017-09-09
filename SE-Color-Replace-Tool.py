695ed8ac298ed85e4f6f8160fe4e999079f14320
Brandon Osterhout <brandonoyster@gmail.com>
2017-09-09 14:56:12 -0400

    Update flow of program in terminal. Added requirments.txt. Added app.ico

--- SE-Color-Replace-Tool.py
+++ SE-Color-Replace-Tool.py
@@ -49,14 +49,14 @@ def getCubeColorID(cubeColors, block):
 def doColorChange( bpPath):
 	# Normalize the file path so that it works on windows without causing escape character issues
 	bpFilePath = os.path.normpath( bpPath + '/bp.sbc' )
-	print( bpFilePath )
+	print( '.sbc Filepath:\n',bpFilePath )
 	tree = ET.parse( bpFilePath)
 	root = tree.getroot()
 
 	# Make a backup of the current tree with 'back_' prefixed
 	# tree.write( bpPath + "bp.sbc_backup" )
 
-	print(root.tag)
+	# print(root.tag)
 
 	shipBPs = root[0]
 	shipBP = shipBPs[0]
@@ -79,21 +79,26 @@ def doColorChange( bpPath):
 	print("Colors:\n")
 	index = 0
 	for b in cubeColors:
-		print(index,"\t",'|hue (x): ',b[0].attrib['x'],
-		'\t|saturation (y):',
-		b[0].attrib['y'],'\t\t|value (z):',b[0].attrib['z'])
+		print(index,'{:<18}'.format(' | hue (x): ' + b[0].attrib['x']),end='')
+		print('{:<24}'.format('| saturation (y): ' + b[0].attrib['y']),end='')
+		print('{:<18}'.format('| value (z): ' + b[0].attrib['z']),end='\n')
 		
 		index += 1
 
 	# Print a blank line and request input for the new color choice
 	print()
-	num = input("Type the number of the color to replace/ exit: ")
-	if num == "exit":
-	    return 2
+	num = input("Type the index of the color to replace or 'e' to exit: ")
+	if num.lower() == 'exit' or num.lower() == 'e':
+		print()
+		return 2
 
-	x = input("New Hue(x):")
-	y = input("New Saturation(Y):")
-	z = input("New Value (Z):")
+	x = input("New Hue(x): ")
+	y = input("New Saturation(Y): ")
+	z = input("New Value (Z): ")
+	print('\nNew Color:')
+	print(index,'{:<18}'.format(' | hue (x): ' + cubeColors[int(num)][0].attrib['x']),end='')
+	print('{:<24}'.format('| saturation (y): ' + cubeColors[int(num)][0].attrib['y']),end='')
+	print('{:<18}'.format('| value (z): ' + cubeColors[int(num)][0].attrib['z']),end='\n')
 
 	# set the hsv_mask attribute in all of the blocks found with the previous color
 	for block in cubeColors[int(num)]:
@@ -110,7 +115,7 @@ def doColorChange( bpPath):
 		os.remove(os.path.normpath(bpPath +'/old_bp.sbc'))
 		os.remove(os.path.normpath(bpPath +'/old_bp.sbcPB'))
 	except:
-		print("blarg")
+		print("No backups exist, making new ones.")
 
 	try:
 		os.rename(os.path.normpath( bpPath+'/bp.sbcPB'), os.path.normpath(bpPath+'/old_bp.sbcPB'))
@@ -123,27 +128,33 @@ def doColorChange( bpPath):
 
 	# Change the name of the new, modified blueprint back to bp.sbc
 	os.rename(os.path.normpath( bpPath+'/bpn.sbc'), os.path.normpath( bpPath +'/bp.sbc'))
-
+	print()
 	return 0
 # ----------------- Main Program ------------------------ 
 
-bpName = input("Please type in the name of the Blueprint you wish to modify:")
+bpName = input("\nPlease type in the name of the Blueprint you wish to modify: ")
+print()
 appDataPath = os.getenv('APPDATA')
 bpPath = appDataPath + '/'+'SpaceEngineers/Blueprints/local/' + bpName
 bpPath = os.path.normpath( bpPath )
 exit = False
+print('Blueprint Folder Location:\n', bpPath + '\\\n')
 
 if not os.path.isdir( bpPath ):
-	print( "Error: Blueprint Doesn't Exist" )
+	print( "Error: Blueprint Doesn't Exist\n" )
 	exit = True
 else:
 	while( exit == False ):
-		userInput = input( "Replace Color / Exit (R/E):")
-		if userInput == 'R':
+		print("0: Edit (E)")
+		print("1: quit/exit (q)\n")
+		userInput = input( "--> ")
+		if userInput.lower() == 'e' or userInput.lower() == 'edit' or userInput.lower() == '0':
+			print()
 			returnValue = doColorChange ( bpPath )
 			if returnValue == 2:
 			    break
-		elif userInput == "E" or userInput =="exit":
+		elif userInput.lower() == "q" or userInput.lower() =="exit" or userInput.lower() == "quit" or userInput.lower() == '0':
+			print()
 			exit = True
 			break
 
